3817d2529b87712d60be2fb247dd5132cacdad05
Pietertju <p.ebbers@student.tue.nl>
2023-12-07 07:44:24 +0100

    Cleaned up code

--- Day07/Day7.java
+++ Day07/Day7.java
@@ -1,21 +1,13 @@
-
 package Day07;
 
-
-
-
 import Benchmarking.Benchmark;
-import static Day04.Day4.contains;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import static java.util.Map.Entry.comparingByValue;
-import java.util.Scanner;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
@@ -31,7 +23,6 @@ public class Day7 {
         long answerPart1 = 0;
         long answerPart2 = 0;
         
-        int lines = 0;
         
         ArrayList<Hand> hands = new ArrayList<>();
         
@@ -50,21 +41,37 @@ public class Day7 {
             System.out.println(e.toString());
         }
         
-        hands.sort(Comparator.comparing(Hand::getRank)
-                .thenComparing(Hand::get0Index)
-                .thenComparing(Hand::get1Index)
-                .thenComparing(Hand::get2Index)
-                .thenComparing(Hand::get3Index)
-                .thenComparing(Hand::get4Index)
+        // part 1
+        hands.sort(Comparator.comparing((Hand hand) -> hand.getRank(false))
+                .thenComparing(hand -> hand.getIndex(0, false))
+                .thenComparing(hand -> hand.getIndex(1, false))
+                .thenComparing(hand -> hand.getIndex(2, false))
+                .thenComparing(hand -> hand.getIndex(3, false))
+                .thenComparing(hand -> hand.getIndex(4, false))
         );
         
-        //Collections.reverse(hands);
         
         int multiplier = 0;
         for(Hand hand : hands) {
             multiplier++;
             answerPart1 += multiplier * hand.bet;
-            System.out.println(hand.hand + "  " + hand.getRank() + "  -  " + multiplier);
+            //System.out.println(hand.hand + "  " + hand.getRank(true) + "  -  " + multiplier);
+        }
+        
+        // part 2
+        hands.sort(Comparator.comparing((Hand hand) -> hand.getRank(true))
+                .thenComparing(hand -> hand.getIndex(0, true))
+                .thenComparing(hand -> hand.getIndex(1, true))
+                .thenComparing(hand -> hand.getIndex(2, true))
+                .thenComparing(hand -> hand.getIndex(3, true))
+                .thenComparing(hand -> hand.getIndex(4, true))
+        );
+        
+        
+        multiplier = 0;
+        for(Hand hand : hands) {
+            multiplier++;
+            answerPart2 += multiplier * hand.bet;
         }
         
         long endTime = Benchmark.currentTime();
@@ -79,62 +86,69 @@ public class Day7 {
     public static class Hand {
         String hand;
         long bet;
-        char most;
-        long more;
+
         public Hand(String hand, long bet) {
             this.hand = hand;
             this.bet = bet;
         }
         
-        public int getRank() {
-            String withoutJokers = this.hand.replace("J","");
-            int withoutLength = withoutJokers.length();
-            if(withoutLength == 0) return 7;
-            long c = withoutJokers.chars().mapToObj(x -> (char) x)
+        public int getRank(boolean jokers) {
+            String copyOfHand = this.hand;
+            if(jokers) {
+                this.hand = this.hand.replace("J","");
+                int withoutLength = this.hand.length();
+                if(withoutLength == 0) {
+                    this.hand = copyOfHand;
+                    return 7;
+                }
+            }
+            
+            long c = this.hand.chars().mapToObj(x -> (char) x)
                             .collect(groupingBy(x -> x, counting()))
                             .entrySet()
                             .stream()
                             .max(comparingByValue()).get().getValue();
-            c+= (5-withoutJokers.length());
-            this.more = c;
-            char character = withoutJokers.chars().mapToObj(x -> (char) x)
+            c += (5-this.hand.length());
+
+            char character = this.hand.chars().mapToObj(x -> (char) x)
                             .collect(groupingBy(x -> x, counting()))
                             .entrySet()
                             .stream()
                             .max(comparingByValue()).get().getKey();
-            this.most = character;
-            String hand2 = withoutJokers.replace(Character.toString(character), "");
+
+            String hand2 = this.hand.replace(Character.toString(character), "");
+            
+            int rank = -1;
             
             if(c == 5) {
-                return 7;
+                rank = 7;
             } else if(c==4) {
-                return 6;
+                rank = 6;
             } else if (c==3) {
-                long d = withoutJokers.chars().mapToObj(x -> (char) x)
+                long d = this.hand.chars().mapToObj(x -> (char) x)
                             .collect(groupingBy(x -> x, counting()))
                             .entrySet()
                             .stream()
                             .min(comparingByValue()).get().getValue();
-                if(d == 2) {
-                    return 5;
-                }
-                return 4;
+                if(d == 2) rank = 5;
+                else rank = 4;
             } else if (c == 2) {
                 long d = hand2.chars().mapToObj(x -> (char) x)
                             .collect(groupingBy(x -> x, counting()))
                             .entrySet()
                             .stream()
                             .max(comparingByValue()).get().getValue();
-                if(d == 2) return 3;
-                return 2;
+                if(d == 2) rank = 3;
+                else rank = 2;
             } else if (c == 1) {
-                return 1;
-            } else {
-                return 0;
+                rank = 1;
             }
+            
+            this.hand = copyOfHand;
+            return rank;
         }
         
-        public int getIndex(int index) {
+        public int getIndex(int index, boolean joker) {
             char c = this.hand.charAt(index);
             if(Character.isDigit(c)) {
                 return Character.getNumericValue(c);
@@ -142,42 +156,15 @@ public class Day7 {
                 if(c == 'A') return 14;
                 if(c == 'K') return 13;
                 if(c == 'Q') return 12;
-                if(c == 'J') return 1;
+                if(c == 'J') {
+                    if(joker) return 1;
+                    return 11;
+                }
                 if(c == 'T') return 10;
             }
             
             return -1;
         }
-        public int get0Index() {
-            int index = 0;
-            return getIndex(index);
-        }
-        public int get1Index() {
-            int index = 1;
-            return getIndex(index);
-        }
-        public int get2Index() {
-            int index = 2;
-            return getIndex(index);
-        }public int get3Index() {
-            int index = 3;
-            return getIndex(index);
-        }public int get4Index() {
-            int index = 4;
-            return getIndex(index);
-        }
-       
-
-//        @Override
-//        public int compareTo(Hand o){
-//            return Comparator.comparing(Hand::getRank)
-//                      .thenComparing(Hand::get0Index)
-//                        .thenComparing(Hand::get1Index)
-//                        .thenComparing(Hand::get2Index)
-//                        .thenComparing(Hand::get3Index)
-//                        .thenComparing(Hand::get4Index)
-//                      .compare(o, this);
-//        }
     }
 }
 
