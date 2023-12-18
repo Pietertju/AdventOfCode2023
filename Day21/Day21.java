7d07cd174017ff95fc9e956c5e6289b1cacead77
Pietertju <p.ebbers@student.tue.nl>
2023-12-18 22:51:38 +0100

    Added skeleton for day19 and on

---
+++ Day21/Day21.java
@@ -0,0 +1,39 @@
+package Day21;
+
+import Benchmarking.Benchmark;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Pieter
+ */
+public class Day21 {
+    public static void main(String[] args) {
+        long startTime = Benchmark.currentTime();
+        File file = new File("res/day21/test.txt");
+        
+        long answerPart1 = 0;
+        long answerPart2 = 0;
+                
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+                     
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                
+            }                     
+        } catch(IOException e) {
+            System.out.println(e.toString());
+        }  
+        
+        long endTime = Benchmark.currentTime();
+        long elapsed = Benchmark.elapsedTime(startTime, endTime);
+     
+        System.out.println("Part 1: " + answerPart1);
+        System.out.println("Part 2: " + answerPart2);
+        System.out.println("Part 1 and 2 took: " + elapsed + " ms combined");
+    }
+}
