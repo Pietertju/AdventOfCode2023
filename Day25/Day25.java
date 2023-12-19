472d20eeb758aa30962684b1138f43df05c91aea
Pietertju <p.ebbers@student.tue.nl>
2023-12-19 13:49:45 +0100

    Changed template for seperate part1 and part2 timings

--- Day25/Day25.java
+++ Day25/Day25.java
@@ -17,23 +17,37 @@ public class Day25 {
         
         long answerPart1 = 0;
         long answerPart2 = 0;
-                
+                        
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
-                     
             while ((line = br.readLine()) != null) {
                 line = line.trim();
-                
-            }                     
+            }
         } catch(IOException e) {
             System.out.println(e.toString());
-        }  
+        }
+        
+        
+        long parseEnd = Benchmark.currentTime();
+        
+        //Part 1
+        answerPart1 = 0;
+        
+        long betweenTime = Benchmark.currentTime();
+        
+        //Part 2
+        answerPart2 = 0;
         
         long endTime = Benchmark.currentTime();
-        double elapsed = Benchmark.elapsedTime(startTime, endTime);
+        
+        double elapsedParse = Benchmark.elapsedTime(startTime, parseEnd);
+        double elapsedPart1 = Benchmark.elapsedTime(parseEnd, betweenTime);
+        double elapsedPart2 = Benchmark.elapsedTime(betweenTime, endTime);
      
         System.out.println("Part 1: " + answerPart1);
         System.out.println("Part 2: " + answerPart2);
-        System.out.println("Part 1 and 2 took: " + elapsed + " ms combined");
+        System.out.println("Parsing input took: " + elapsedParse + " ms");
+        System.out.println("Part 1 took: " + elapsedPart1 + " ms");
+        System.out.println("Part 2 took: " + elapsedPart2 + " ms");
     }
 }
